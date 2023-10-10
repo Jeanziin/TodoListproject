31d8d40de5c0d3706d6b15e65f8d29396c7ade24
Jeanziin <jjeanfreitass@gmail.com>
2023-10-10 01:49:06 -0300

    first commit

---
+++ src/main/java/br/com/api/todolist/user/UserModel.java
@@ -0,0 +1,14 @@
+package br.com.api.todolist.user;
+
+public class UserModel {
+    private String username;
+    private String name;
+    private String password;
+   
+   
+   
+    public String getName() {
+        return name;
+    }
+    
+}
